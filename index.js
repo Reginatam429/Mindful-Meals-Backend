2ef45ffcec51f2b7f69b5e09b4674d6551c9c400
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-04-04 21:49:08 -0700

    Yelp Api is Added tot index.js, and server/routes/uelp.js created

--- index.js
+++ index.js
@@ -2,12 +2,43 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const yelpRoutes = require('./server/routes/yelp.js');
+import axios from 'axios';
 
 const app = express();
+const YELP_API_KEY = process.env.YELP_API_KEY;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+  }));
+  
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.get('/api/search', async (req, res) => {
+    console.log(`Search request received: term=${term}, location=${location}`);
+
+    const { term, location } = req.query;
+  
+    try {
+      const response = await axios.get('https://api.yelp.com/v3/businesses/search', {
+        headers: {
+          Authorization: `Bearer ${YELP_API_KEY}`,
+        },
+        params: {
+          term,
+          location,
+          limit: 10,
+        },
+      });
+  
+      res.json(response.data.businesses);
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+      res.status(500).json({ error: 'Failed to fetch data from Yelp API' });
+    }
+  });
 
 // Test route
 app.get('/', (req, res) => {
