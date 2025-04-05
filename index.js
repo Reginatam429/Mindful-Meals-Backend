a427e529e469f1607d3637b944467d7d389fbecc
Regina <reginatam429@gmail.com>
2025-04-04 18:58:43 -0700

    Add auth route and completed postman testing

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 // Test route
 app.get('/', (req, res) => {
